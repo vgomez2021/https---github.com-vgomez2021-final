2d03ae7131d58eac485a6d813e64c4c00fee6807
vgomez2021 <86268600+vgomez2021@users.noreply.github.com>
2025-07-17 22:12:57 -0300

    update

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="container mt-4">
-      <h1>404 - Página no encontrada</h1>
-      <p>La página que estás buscando no existe.</p>
-      <section className="notfound"></section>
-    </div>
-  );
-}
